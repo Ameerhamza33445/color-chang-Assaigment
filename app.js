a2bf77c369758696094613e45fcfb284e5c1e4d5
unknown <hamzasheikh33445@gmail.com>
2023-12-15 09:26:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function btn(HAMZA) {
+    document.getElementById("HAMZA").style.color = 'red'
+}
+
+
+function btn1(HAMZA) {
+    document.getElementById("HAMZA1").style.color = 'blue'
+}
+
+
+function btn2(HAMZA) {
+    document.getElementById("HAMZA2").style.color = 'pink'
+}
